9bc3ec54753b1cb5a1456d6947128e46a102f4f8
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-04-28 02:05:19 +0100

    Demo

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,10 @@
-let () = print_endline "Hello, World!"
+open Xlib
+
+let set_status dpy msg =
+  xStoreName ~dpy ~win:(xDefaultRootWindow ~dpy) ~name:msg;
+  xSync ~dpy ~discard:false
+
+let () =
+  let dpy = open_display () in
+  set_status dpy "Hello, World!";
+  xCloseDisplay ~dpy
